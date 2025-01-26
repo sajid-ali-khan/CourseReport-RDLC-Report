8cee78c19d7fcd611d8571b9891ce57c92793281
sajid@windows <sajid@windows>
2025-01-26 07:31:13 +0530

    Add project files.

---
+++ CourseReport/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CourseReport._Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div style="padding: 15px">
+        <div align="center" style="font-size: 30px;">Course Info</div>
+        <br />
+        <div align="center">
+            <asp:Button ID="Button1" runat="server" Text="Load Course Details" Width="164px" OnClick="Button1_Click" BackColor="#FF66CC" />
+            <br />
+            <br />
+            Branch: <asp:TextBox ID="et_branch" runat="server"></asp:TextBox>
+&nbsp;Sem:
+            <asp:TextBox ID="et_sem" runat="server"></asp:TextBox>
+&nbsp;SubjectCode:
+            <asp:TextBox ID="et_subjectCode" runat="server"></asp:TextBox>
+            <br />
+            <br />
+        </div>
+        <div align="center">
+            <rsweb:ReportViewer align="center" ID="ReportViewer1" runat="server" Width="768px"></rsweb:ReportViewer>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ CourseReport/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
+using Microsoft.Reporting.WebForms;
+
+namespace CourseReport
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-PJ0OUAF\\SQLEXPRESS;Initial Catalog=RDLC_Report;Integrated Security=True");
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string branch = et_branch.Text;
+            string sem = et_sem.Text;
+            string scode = et_subjectCode.Text;
+            StringBuilder query = new StringBuilder();
+            query.Append("SELECT * FROM course");
+
+            List<string> conditions = new List<string>();
+
+            if (!string.IsNullOrEmpty(branch))
+            {
+                conditions.Add($"branch = '{branch}'");
+            }
+            if(!string.IsNullOrEmpty(sem))
+            {
+                conditions.Add($"sem = '{sem}'");
+            }
+            if (!string.IsNullOrEmpty(scode))
+            {
+                conditions.Add($"subject_code = '{scode}'");
+            }
+
+            if (conditions.Count > 0)
+            {
+                query.Append(" WHERE ");
+                query.Append(string.Join( " AND ", conditions));
+            }
+
+
+            SqlCommand command = new SqlCommand(query.ToString(), con);
+            SqlDataAdapter s = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            s.Fill(dt);
+
+            ReportViewer1.LocalReport.DataSources.Clear();
+            ReportDataSource rds = new ReportDataSource("DataSet1", dt);
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("Report1.rdlc");
+            ReportViewer1.LocalReport.DataSources.Add(rds);
+            ReportViewer1.LocalReport.Refresh();
+        }
+    }
+}
